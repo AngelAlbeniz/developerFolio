3c715c3a9e66391c6570f62d9fde0906b44f767b
AngelAlbeniz <48287605+AngelAlbeniz@users.noreply.github.com>
2021-01-08 22:24:29 +0100

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -12,7 +12,7 @@ const greeting = {
   username: 'Angel Albeniz',
   title: "Hi all, I'm Angel",
   subTitle: emoji(
-    'A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks.'
+    'PAGINA EN CONSTRUCION!! 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks.'
   ),
   resumeLink:
     'https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing',
