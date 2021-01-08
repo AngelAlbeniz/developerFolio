2c225151f917045e225ffabf69cc6093d640edb2
AngelAlbeniz <48287605+AngelAlbeniz@users.noreply.github.com>
2021-01-08 22:21:25 +0100

    Fotos Logos

--- src/portfolio.js
+++ src/portfolio.js
@@ -149,10 +149,10 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: 'Software Engineer',
-      company: 'Facebook',
-      companylogo: require('./assets/images/facebookLogo.png'),
-      date: 'June 2018 – Present',
+      role: 'Chief Engineer',
+      company: 'HelixNorth',
+      companylogo: require('./assets/images/HelixNorthLogo.png'),
+      date: 'April 2018 – Present',
       desc:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       descBullets: [
@@ -161,18 +161,18 @@ const workExperiences = {
       ],
     },
     {
-      role: 'Front-End Developer',
-      company: 'Quora',
-      companylogo: require('./assets/images/UPNALogo.png'),
-      date: 'May 2017 – May 2018',
+      role: 'Lecturer',
+      company: 'AIN',
+      companylogo: require('./assets/images/AINLogo.png'),
+      date: 'May 2019 – June 2019',
       desc:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
     {
-      role: 'Software Engineer Intern',
-      company: 'Airbnb',
-      companylogo: require('./assets/images/UPNALogo.png'),
-      date: 'Jan 2015 – Sep 2015',
+      role: 'Lecturer',
+      company: 'CIP Donapea',
+      companylogo: require('./assets/images/DonapeaLogo.png'),
+      date: 'May 2018 – June 2018',
       desc:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
