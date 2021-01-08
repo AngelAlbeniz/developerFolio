b53a9614ce9ff15c9a7e2d2d4f95f679a6392765
AngelAlbeniz <48287605+AngelAlbeniz@users.noreply.github.com>
2021-01-08 17:28:11 +0100

    Elminate not used photos

--- src/portfolio.js
+++ src/portfolio.js
@@ -114,8 +114,8 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: 'Harvard University',
-      logo: require('./assets/images/harvardLogo.png'),
+      schoolName: 'UPNA',
+      logo: require('./assets/images/UPNALogo.png'),
       subHeader: 'Master of Science in Computer Science',
       duration: 'September 2017 - April 2019',
       desc: 'Participated in the research of XXX and published 3 papers.',
